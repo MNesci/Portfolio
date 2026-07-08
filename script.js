d3ba0c13f32c4599e4bd1d13a2539ea62d7c740d
Mike Nesci <mikenesci@hotmail.com>
2026-07-08 14:21:06 -0400

    Update portfolio site to reflect new experience and skills

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
-// Scroll to Main when the Learn More button in the header is clicked
-// assign button to a variable
-const scrollButton = document.querySelector('.learnMoreButton');
+// Header Buttons
+let headerButtons = document.querySelectorAll('.headerButtonContainer');
+for (let i = 0; i < headerButtons.length; i++) {
+    let button = headerButtons[i];
+    button.addEventListener('click', () => headerScroll(button));
+};
 
-// assign main to a variable
-const mainElement = document.querySelector('main');
+// Header Logo as button
+let headerLogo = document.querySelector('.headerLogo');
+headerLogo.addEventListener('click', () => headerScroll(headerLogo));
 
-// function that is triggered when Leran More button is clicked
-scrollButton.addEventListener('click', function(click) {
-    // Scroll to main
-    mainElement.scrollIntoView();  
-});
-
-// Scroll to Contact Section when the Get in Touch button in the header is clicked
-// assign button to a variable
-const contactButton = document.querySelector('.contactButton');
-
-// assign Contact Section to a variable
-const contactSection = document.querySelector('.contactSection');
-
-// function that is triggered when Get in Touch button is clicked
-contactButton.addEventListener('click', function(click) {
-    // Scroll to Contact Section
-    contactSection.scrollIntoView();
-});
-
-// Scroll to Top when the footer card is clicked
-// assign footer card to a variable
-const footerCard = document.querySelector('.footerCard');
-
-// assign header to a variable
-const headerSection = document.querySelector('header');
-
-// function that is triggered when footer card is clicked
-footerCard.addEventListener('click', function(click) {
-    // Scroll to header
-    headerSection.scrollIntoView();
-});
+function headerScroll(button) {
+    let id = button.id;
+    let targetId = id.replace('Button', 'Section');
+    let target = document.querySelector(`#${targetId}`);
+    console.log(target)
+    target.scrollIntoView();
+};
